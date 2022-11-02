82575922cdf79b4574d278f6fcc4b782490553aa
krystian.wojda <krystian.wojda-98@wp.pl>
2022-11-02 14:25:49 +0100

    Stworzenie komponentu karty który bedzie wokół formularza i dodanie do niego styli

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,21 +1,24 @@
 import React from "react";
+import Card from "../UI/Card";
 
 const AddUser = props => {
     const addUserHandler = (event) => {
         event.preventDefault();
     };
     return(
-        <form onSubmit={addUserHandler}>
-            <label htmlFor="username">Username</label>
-            <input className="username" type="text"/>
-            <label htmlFor="userage" >Age (years)</label>
-            <input className="userage" type="number"/>
-            <label htmlFor="userheight" >Height (cm)</label>
-            <input className="userheight" type="number"/>
-            <label htmlFor="userheight">Weight (kg)</label>
-            <input className="userweight" type="number"/>
-            <button type="submit">Add User</button>
-        </form>
+        <Card>
+            <form onSubmit={addUserHandler}>
+                <label htmlFor="username">Username</label>
+                <input className="username" type="text"/>
+                <label htmlFor="userage" >Age (years)</label>
+                <input className="userage" type="number"/>
+                <label htmlFor="userheight" >Height (cm)</label>
+                <input className="userheight" type="number"/>
+                <label htmlFor="userheight">Weight (kg)</label>
+                <input className="userweight" type="number"/>
+                <button type="submit">Add User</button>
+            </form>
+        </Card>
     );
 };
 
