b36595a23541c857c20833d427c79ef42fae5f36
krystian.wojda <krystian.wojda-98@wp.pl>
2022-11-03 12:24:24 +0100

    Podpiecię karty do nowego komponentu z użytkownikami

--- src/components/Users/UserList.js
+++ src/components/Users/UserList.js
@@ -1,19 +1,23 @@
 import React from "react";
 
-import "./UserList.module.css";
+import Card from "../UI/Card";
+import classes from "./UserList.module.css";
+import card from "../UI/Card";
 
 const UserList = (props) => {
     return(
-        <ul>
-            {props.users.map((user) => (
-                <li>
-                    {user.name}
-                    ({user.age} years old)
-                    ({user.height} cm)
-                    ({user.weight} kg)
-                </li>
-            ))}
-        </ul>
+        <Card className={classes.users}>
+            <ul>
+                {props.users.map((user) => (
+                    <li>
+                        {user.name}
+                        ({user.age} years old)
+                        ({user.height} cm)
+                        ({user.weight} kg)
+                    </li>
+                ))}
+            </ul>
+        </Card>
     );
 };
 
